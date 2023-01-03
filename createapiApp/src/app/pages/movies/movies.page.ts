77ad7f65f81e96717684dce6b2f09a1590605718
Jethva Dev Rajinikantbhai <devjethva235@gmail.com>
2023-01-03 13:48:12 +0530

    CreateApi

---
+++ createapiApp/src/app/pages/movies/movies.page.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MovieService, SearchType } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+
+  results: Observable<any>;
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+  }
+
+  searchChanged() {
+    // Call our service function which returns an Observable
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
+  }
+}
